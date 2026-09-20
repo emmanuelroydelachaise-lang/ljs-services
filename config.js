14f76f8a90ee4cc1d2c25f76969767a64ad9f7dd
emmanuelroydelachaise-lang <emmanuel.roydelachaise@gmail.com>
2026-09-20 08:36:57 +0200

    Connect LJS Services to central Supabase

---
+++ config.js
@@ -0,0 +1,5 @@
+window.LJS_SERVICES_CONFIG = {
+  SUPABASE_URL: "https://wptorwbmgwsadbuxwyvk.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_VO7mh6MsjflCc8IIUpElPA_8rAaJo89",
+  APP_CODE: "ljs-services"
+};
